b8c42cfca36d8079005342953f977bab8387bc90
Marc Vouve <mvouve@gmail.com>
2015-03-29 16:25:43 -0700

    Added implementation for Burger views.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,7 +30,7 @@ class Welcome extends Application {
         {
             $ordr = new stdclass();
             $ordr->order    = $order->get_order_name();
-            $ordr->url      = 'welcom/order/' . $order->get_order_name();
+            $ordr->url      = 'welcome/order/' . $order->get_order_name();
             $ordr->customer = $order->get_customer_name(); 
             
             $this->data['orders'][] = $ordr;
@@ -48,8 +48,39 @@ class Welcome extends Application {
 
     function order($filename)
     {
+        $this->load->model('orders');
 	// Build a receipt for the chosen order
-	
+	$order = $this->orders->get($filename);
+        $burgers = $order->get_burgers();
+        $this->data['burgers'] = array();
+        
+        $i = 0;
+        foreach($burgers as $burger)
+        {
+            $burger_->num = $i++;
+            $burger_->patty = $burger->get_patty();
+            $burger_->cheeses = $burger_->get_cheeses();
+            $burger_->toppings = array();
+            
+            foreach($burger->get_toppings() as $t)
+            {
+                $burger_->toppings[] = $t;
+            }
+            
+            $burger_['sauces'] = array();
+            
+            foreach($burger->get_sauces() as $t)
+            {
+                $burger_->sauces[] = $t;
+            }
+            setlocale(LC_MONETARY, 'en_CA');
+            $burger_['price'] = money_format('#n', $burger->get_total());
+            
+            $this->data['burgers'][] = $burger_;
+        }
+        
+        
+        
 	// Present the list to choose from
 	$this->data['pagebody'] = 'justone';
 	$this->render();
